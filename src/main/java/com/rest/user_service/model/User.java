da195334a18f8a7e25b2cf639348ebbe8247ca25
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2026-05-05 20:21:10 +0530

    auth: added implementations for UserDetails and UserDetailsService interfaces

--- src/main/java/com/rest/user_service/model/User.java
+++ src/main/java/com/rest/user_service/model/User.java
@@ -9,15 +9,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
 
     @NotNull
     @Size(min = 3, message = "username needs to be at least 3 characters")
@@ -31,5 +36,16 @@ public class User {
     @Email
     @Size(min = 5, message = "email needs to be at least 5 characters")
     private String email;
+
+    @ManyToMany(
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 
 }
--- src/main/java/com/rest/user_service/security/SecurityConfig.java
+++ src/main/java/com/rest/user_service/security/SecurityConfig.java
@@ -1,4 +1,63 @@
+
 package com.rest.user_service.security;
 
+import com.rest.user_service.security.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    @Order(SecurityFilterProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+
+        http
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/*").permitAll()
+                                .anyRequest()
+                                .authenticated()
+                );
+
+        http.authenticationProvider(authenticationProvider());
+
+//        http.exceptionHandling(exception -> exception.authenticationEntryPoint());
+//        http.addFilterBefore();
+
+        return http.build();
+
+    }
+
 }
