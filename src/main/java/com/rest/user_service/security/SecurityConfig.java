9c146e987798194e0ae9c62d4f7a9bce0673b40d
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2026-05-06 16:27:42 +0530

    docs: added swagger api docs

--- src/main/java/com/rest/user_service/security/SecurityConfig.java
+++ src/main/java/com/rest/user_service/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -59,7 +60,9 @@ public class SecurityConfig {
                         auth
                                 .requestMatchers(
                                         "/api/public/**",
-                                        "/api/auth/**"
+                                        "/api/auth/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui/**"
                                 )
                                 .permitAll()
                                 .anyRequest()
@@ -72,7 +75,6 @@ public class SecurityConfig {
                 exception -> exception.authenticationEntryPoint(unauthorizedHandler)
         );
         http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
     }
 }
